e792d3abcb2c3b834de6ed9b18cc45295dc92fe3
soulmachine <soulmachine@gmail.com>
2020-01-10 05:51:48 -0800

    Added  Bitfinex name mapping

--- src/exchange/bitfinex.ts
+++ src/exchange/bitfinex.ts
@@ -3,19 +3,40 @@ import axios from 'axios';
 import { ExchangeInfo } from '../pojo/exchange_info';
 import { BitfinexPairInfo, convertArrayToMap, PairInfo } from '../pojo/pair_info';
 
-function extractNormalizedPair(pairInfo: PairInfo): string {
+async function getNameMapping(): Promise<{ [key: string]: string }> {
+  const response = await axios.get('https://api-pub.bitfinex.com/v2/conf/pub:map:currency:sym');
+  const arr = response.data[0] as [string, string][];
+  const result: { [key: string]: string } = {};
+  arr.forEach(x => {
+    const [key, value] = x;
+    result[key] = value;
+  });
+  return result;
+}
+
+function extractNormalizedPair(pairInfo: PairInfo, mapping: { [key: string]: string }): string {
   const rawPair = pairInfo.raw_pair.toUpperCase();
   if (rawPair.includes(':')) {
     return rawPair.replace(/:/g, '_').toUpperCase();
   }
-  return `${rawPair.substring(0, rawPair.length - 3)}_${rawPair.substring(rawPair.length - 3)}`;
+
+  let baseSymbol = rawPair.substring(0, rawPair.length - 3);
+  if (baseSymbol in mapping) baseSymbol = mapping[baseSymbol];
+
+  let quoteSymbol = rawPair.substring(rawPair.length - 3);
+  if (quoteSymbol in mapping) quoteSymbol = mapping[quoteSymbol];
+
+  return `${baseSymbol}_${quoteSymbol}`;
 }
 
 /* eslint-disable no-param-reassign */
-function populateCommonFields(pairInfo: BitfinexPairInfo): void {
+function populateCommonFields(
+  pairInfo: BitfinexPairInfo,
+  mapping: { [key: string]: string },
+): void {
   pairInfo.exchange = 'Bitfinex';
   pairInfo.raw_pair = pairInfo.pair;
-  pairInfo.normalized_pair = extractNormalizedPair(pairInfo);
+  pairInfo.normalized_pair = extractNormalizedPair(pairInfo, mapping);
   pairInfo.base_precision = 8; // see https://github.com/bitfinexcom/bfx-api-node-util/blob/master/lib/precision.js
   pairInfo.quote_precision = pairInfo.price_precision;
   pairInfo.min_base_quantity = parseFloat(pairInfo.minimum_order_size);
@@ -26,9 +47,11 @@ export async function getPairs(): Promise<{ [key: string]: PairInfo }> {
   const response = await axios.get('https://api.bitfinex.com/v1/symbols_details');
   assert.equal(response.status, 200);
   assert.equal(response.statusText, 'OK');
+
+  const mapping = await getNameMapping();
   const arr = response.data as BitfinexPairInfo[];
 
-  arr.forEach(p => populateCommonFields(p));
+  arr.forEach(p => populateCommonFields(p, mapping));
 
   return convertArrayToMap(arr);
 }
